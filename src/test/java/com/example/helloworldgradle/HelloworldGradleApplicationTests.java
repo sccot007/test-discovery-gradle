5a9ef745e1db6e8036fe25bda29710aec2bd14a3
sccot007 <islandkhj@gmail.com>
2025-04-03 14:28:18 +0900

    init

---
+++ src/test/java/com/example/helloworldgradle/HelloworldGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
